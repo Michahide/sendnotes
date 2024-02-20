9439dff586a82bf9e8f3dd5e8db9ee3db61df7a8
Michael Mervin Ruswan <202310016@student.ibik.ac.id>
2024-02-20 09:57:43 +0700

    create note model

---
+++ app/Models/Note.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = [
+        'id',
+        // 'created_at',
+        // 'updated_at',
+    ];
+
+    // protected $casts = [
+    //     'send_date' => 'date',
+    //     'is_published' => 'boolean',
+    // ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Assign a super user
+    // public function superUser()
+    // {
+    //     return $this->belongsTo(User::class, 'user_id', 'id');
+    // }
+
+}
