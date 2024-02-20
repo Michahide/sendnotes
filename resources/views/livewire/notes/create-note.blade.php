54c47aa9b412b61e9716780d4dbfe6ac970bd952
Michael Mervin Ruswan <202310016@student.ibik.ac.id>
2024-02-20 10:24:57 +0700

    add create note backend

--- resources/views/livewire/notes/create-note.blade.php
+++ resources/views/livewire/notes/create-note.blade.php
@@ -8,37 +8,45 @@ new class extends Component {
     public $noteRecipient;
     public $noteSendDate;
 
-    /*    public function submit()
-    {
-        $this->validate([
-            'noteTitle' => 'required',
-            'noteBody' => 'required',
-            'noteRecipient' => 'required',
-            'noteSendDate' => 'required',
-        ]);
-
-        $note = Auth::user()->notes()->create([
-            'title' => $this->noteTitle,
-            'body' => $this->noteBody,
-            'recipient' => $this->noteRecipient,
-            'send_date' => $this->noteSendDate,
-        ]);
-
-        $this->emit('noteCreated', $note->id);
-    }
-*/
     public function submit()
+    {
+        $validated = $this->validate([
+            /*'noteTitle' => 'required',*/
+            'noteTitle' => ['required', 'string', 'min:3', 'max:255'],
+            'noteBody' => ['required', 'string', 'min:10'],
+            'noteRecipient' => ['required', 'email'],
+            'noteSendDate' => ['required', 'date'],
+        ]);
+
+        $note = Auth::user()
+            ->notes()
+            ->create([
+                'title' => $this->noteTitle,
+                'body' => $this->noteBody,
+                'recipient' => $this->noteRecipient,
+                'send_date' => $this->noteSendDate,
+                'is_published' => false, //default value for new notes.
+            ]);
+
+        redirect()->route('notes.index');
+        // $this->emit('noteCreated', $note->id);
+    }
+
+    /*    public function submit()
     {
         dd($this->noteTitle, $this->noteBody, $this->noteRecipient, $this->noteSendDate);
     }
+*/
 }; ?>
 
 <div>
-    <form wire:submit='submit'>
-        <x-input wire:model="noteTitle" type="text" label="Title" placeholder="Title" />
-        <x-textarea wire:model="noteBody" label="Body" placeholder="Type your note here..." />
-        <x-input wire:model="noteRecipient" type="text" label="Recipient" placeholder="Recipient" />
-        <x-input wire:model="noteSendDate" type="date" label="Send Date" />
-        <x-button wire:click="submit">Submit</x-button>
+    <form wire:submit='submit' class="space-y-4">
+        <x-input wire:model="noteTitle" type="text" label="Note Title" placeholder="Greetings!" />
+        <x-textarea wire:model="noteBody" label="Body" placeholder="Tell a story or something to your friend" />
+        <x-input icon="user" wire:model="noteRecipient" type="text" label="Recipient"
+            placeholder="Your friend email" />
+        <x-input icon="calendar" wire:model="noteSendDate" type="date" label="Send Date" />
+        <x-button wire:click="submit" primary right-icon="calendar" spinner>Schedule Note</x-button>
+        <x-errors />
     </form>
 </div>
